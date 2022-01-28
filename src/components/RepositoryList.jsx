4e7525683dbf2fd61fc73c3349bdcd645418eeba
LeandroNeto <leandrohock@gmail.com>
2022-01-27 21:59:03 -0300

    Versão Inicial

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,34 @@
+const repositoryName = 'unForm2';
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unForm</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unForm</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
