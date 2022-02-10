433efb5e1e3e337abeb45d807ed58c812505ed93
LeandroNeto <leandrohock@gmail.com>
2022-02-10 08:15:54 -0300

    Conceitos Importantes

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,33 +1,18 @@
-const repositoryName = 'unForm2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+}
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar Repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unForm</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar Repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unForm</strong>
-          <p>Forms in React</p>
-          <a href="">
-            Acessar Repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   )
